ea3ef60d05e2eb5a67e1cc9aac0c8b80e84f5665
Anish Upreti <upretianish407@.com>
2026-02-25 12:14:25 +0545

    changes pathname

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const cp = require('cookie-parser')
 const userRouter = require("./routs/userRouts")
 const chatRouter = require("./routs/chatRoutes")
 const messageRouter = require("./routs/messageRoutes")
+const path = require("path")
 
 const app = express()
-app.use(express.static("./public"))
+app.use(express.static(path.join(__dirname , 'dist')))
 app.use(cors({origin : "http://localhost:5173" , credentials : true}))
 app.use(cp())
 app.use(express.json())
 connectDB()
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use('/api/user', userRouter)
 app.use("/api/chat" , chatRouter)
 app.use("/api/message" , messageRouter)
